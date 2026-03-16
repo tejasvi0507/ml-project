0b9431c84dabe0d3cbdcdd65905c0f9b08ff364a
Tejasvi K <tejasvik0507@gmail.com>
2026-03-16 23:04:56 +0530

    practice program

---
+++ while/ex1.py
@@ -0,0 +1,8 @@
+i = 1
+sum = 0
+
+while i <= 5:
+    sum += i
+    i += 1
+
+print("Sum is:", sum)
---
+++ while/ex3.py
@@ -0,0 +1,7 @@
+num = 12345
+reverse = 0
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num = num // 10
+print("Reversed number is:", reverse)
