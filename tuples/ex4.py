838d79a80874d60a62325361bfdd7c730f88bee2
Tejasvi K <tejasvik0507@gmail.com>
2026-03-17 23:27:59 +0530

    practice program

---
+++ tuples/ex4.py
@@ -0,0 +1,13 @@
+fruits = ("apple", "banana", "mango")
+
+print("First fruit:", fruits[0])
+print("Second fruit:", fruits[1])
+
+print("All fruits:")
+for fruit in fruits:
+    print(fruit)
+
+print("Number of fruits:", len(fruits))
+
+if "apple" in fruits:
+    print("Apple is in the tuple")
