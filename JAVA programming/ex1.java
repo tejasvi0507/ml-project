478b782b505d894a370cf450de46b112e054c3d5
Tejasvi K <tejasvik0507@gmail.com>
2026-03-29 23:27:00 +0530

    Practice Program

---
+++ JAVA programming/ex1.java
@@ -0,0 +1,16 @@
+public class ex1 {
+    public static void main(String[] args) {
+        
+        int age = 20;
+        double marks = 85.5;
+        char grade = 'A';
+        boolean isPass = true;
+        String name = "Tejasvi";
+        
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + isPass);
+    }
+}
