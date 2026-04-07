cbb7737ae6aa7abe3d618c0e4d941c30a4ab848d
Tejasvi K <tejasvik0507@gmail.com>
2026-04-08 04:19:55 +0530

    Array Practice Program

---
+++ JAVA programming/ex6.java
@@ -0,0 +1,12 @@
+public class ex6 {
+    public class Main {
+    public static void main(String[] args) {
+        
+        int[] arr = {10, 20, 30, 40, 50};
+        
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
+}
