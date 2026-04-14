0edc0515f49741a123ffb640aa63b9b0534be5b0
Tejasvi K <tejasvik0507@gmail.com>
2026-04-14 23:25:32 +0530

    Practice Program

---
+++ JAVA programming/ex9.java
@@ -0,0 +1,12 @@
+public class ex9 {
+    public class Main {
+    
+    static void greet() {
+        System.out.println("Hello!");
+    }
+
+    public static void main(String[] args) {
+        greet();  // calling method
+    }
+}
+}
