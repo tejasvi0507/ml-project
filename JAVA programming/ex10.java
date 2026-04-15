55a3d4bac7a6333db6d1ccaaa590514d8bc8eaf2
Tejasvi K <tejasvik0507@gmail.com>
2026-04-15 22:24:33 +0530

    Practice Program

---
+++ JAVA programming/ex10.java
@@ -0,0 +1,12 @@
+public class ex10 {
+    public class Main {
+    
+    static void greet(String name) {
+        System.out.println("Hello " + name);
+    }
+
+    public static void main(String[] args) {
+        greet("Tejasvi");
+    }
+}
+}
