7f0a4047a7a75c3cf21fe5eea7c9fc81b29062a2
Tejasvi K <tejasvik0507@gmail.com>
2026-04-18 22:56:37 +0530

    Practice Program

---
+++ JAVA programming/ex13.java
@@ -0,0 +1,13 @@
+class Student {
+    String name;
+
+    Student() {
+        System.out.println("Constructor called!");
+    }
+}
+
+public class ex13 {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+    }
+}
