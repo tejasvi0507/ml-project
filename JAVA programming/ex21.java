f24cc59cd87e49594f021c188983b5e3b09d0445
Tejasvi K <tejasvik0507@gmail.com>
2026-04-27 23:30:27 +0530

    Practice Program

---
+++ JAVA programming/ex21.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class ex21 {
+    public static void main(String[] args) {
+        
+        try {
+            File file = new File("test.txt");
+            Scanner sc = new Scanner(file);
+            
+            while (sc.hasNextLine()) {
+                String data = sc.nextLine();
+                System.out.println(data);
+            }
+            
+            sc.close();
+        } catch (Exception e) {
+            System.out.println("Error reading file");
+        }
+    }
+}
