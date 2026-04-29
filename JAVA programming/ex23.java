037f13ef0653a16222b97cae4a4f4008198d8ee5
Tejasvi K <tejasvik0507@gmail.com>
2026-04-29 23:12:19 +0530

    Practice Program

---
+++ JAVA programming/ex23.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ex23 {
+    public static void main(String[] args) {
+        
+        ArrayList<String> names = new ArrayList<>();
+        
+        names.add("Tejasvi");
+        names.add("Rahul");
+        names.add("Ankit");
+        
+        System.out.println(names);
+    }
+}
