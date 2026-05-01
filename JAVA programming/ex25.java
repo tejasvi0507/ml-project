04ab42642381422d4fb537ef1484faa24bc1c122
Tejasvi K <tejasvik0507@gmail.com>
2026-05-01 22:53:28 +0530

    Practice Program

---
+++ JAVA programming/ex25.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class ex25 {
+    public static void main(String[] args) {
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        map.put("Tejasvi", 90);
+        map.put("Rahul", 80);
+        map.put("Ankit", 85);
+        
+        System.out.println(map);
+    }
+}
